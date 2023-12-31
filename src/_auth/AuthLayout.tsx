42dcbe2ea8fc22f8557a6e6fa917e7bbebe6eb0e
Syed Dawar Alvi <46403138+DawarAlvi@users.noreply.github.com>
2024-01-01 00:39:54 +0530

    sign up page, create account wip

---
+++ src/_auth/AuthLayout.tsx
@@ -0,0 +1,26 @@
+import {Outlet, Navigate} from 'react-router-dom';
+
+const AuthLayout = () => {
+  const isAuthenticated = false;
+  return (
+    <>
+      {isAuthenticated ? (
+        <Navigate to="/" />
+      ): (
+        <>
+        <section className="flex flex-1 justify-center items-center flex-col py-10">
+          <Outlet />
+        </section>
+
+        <img 
+          src="https://raw.githubusercontent.com/adrianhajdin/social_media_app/9fa032dd198c92aa498db82d248c0d93f28ebf5c/public/assets/images/side-img.svg"
+          className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"
+        />
+
+        </>
+      )}
+    </>
+  )
+}
+
+export default AuthLayout
---
+++ src/components/shared/Loader.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Loader = () => {
+  return (
+    <div className="flex flex-center w-full"><img src="https://raw.githubusercontent.com/adrianhajdin/social_media_app/9fa032dd198c92aa498db82d248c0d93f28ebf5c/public/assets/icons/loader.svg" alt="loader" width={24} height={24} /></div>
+  )
+}
+
+export default Loader
